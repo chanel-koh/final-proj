f4773f239a39874bbd033e776080770a0708c716
chanel-koh <chanelkoh04@gmail.com>
2026-05-06 00:29:34 -0700

    Added database connection functions

--- data_manager.py
+++ data_manager.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
-from local_settings import postgresql as settings
+from local_settings import PostgresConfig as settings
 
 class DataManager:
     def __init__(self, dataset: str, save_path: str) -> None:
@@ -49,11 +49,43 @@ class DataManager:
         return df
     
 class Pipeline:
-    def __init__(self, data_handler: DataManager, db_url: str):
+    def __init__(self, data_handler: DataManager):
         self.data_handler = data_handler
-        self.engine = create_engine(db_url)
+        self.engine = self.get_engine_config_from_settings()
+
+    
+    def get_engine(self, user, passwd, host, port, db):
+        """
+        Creates a db connection with SQLALchemy with user defined pool size (idle connections).
+        """
+        url = f"postgresql://{user}:{passwd}@{host}:{port}/{db}"
+
+        if not database_exists(url):
+            create_database(url)
+        
+        engine = create_engine(url, pool_size=15, echo=False)
+
+        return engine
+    
+    def get_engine_config_from_settings(self):
+        """
+        Gets config information from local settings file to establish db connection.
+        """
+        
+        return self.get_engine(settings.user, settings.passwd, settings.host, settings.port, settings.db)
+    
+    def get_session(self):
+        """
+        Gets current db session.
+        """
+        engine = self.get_engine_config_from_settings()
+        session = sessionmaker(bind=engine)()
+        return session
 
     def run(self, table_name: str) -> None:
+        """
+        Runs the full data seeding pipeline of downloading the external data, cleaning it, and conversion to sql.
+        """
         self.data_handler.download()
         df = self.data_handler.load_and_clean()
         df.to_sql(table_name, self.engine, if_exists="replace", index=False)
---
+++ local_settings.py
@@ -0,0 +1,6 @@
+class PostgresConfig:
+    user = "your_user",
+    passwd =  "your_password",
+    host =  "localhost",
+    port =  "5432",
+    db =  "your_database"
