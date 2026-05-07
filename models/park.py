76d3be391a1b33c2c3d83ff81f1ea63cd2eda67f
chanel-koh <chanelkoh04@gmail.com>
2026-05-06 18:50:37 -0700

    Added identity characteristic to supplemental key for auto increment

--- models/park.py
+++ models/park.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Identity
 from sqlalchemy.orm import relationship
 from database_manager import Base
 
 class Park(Base):
     __tablename__ = "park"
 
-    id = Column(Integer, nullable=False, unique=True)
+    id = Column(Integer, Identity(), nullable=False, unique=True)
     park_name = Column(String, primary_key=True)
     us_state = Column(String, primary_key=True)
     description = Column(String, nullable=False)
--- models/trail.py
+++ models/trail.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, String, ForeignKey, Float, Integer
+from sqlalchemy import Column, String, ForeignKey, Float, Integer, Identity
 from sqlalchemy.orm import relationship
 from database_manager import Base
 
 class Trail(Base):
     __tablename__ = "trail"
 
-    id = Column(Integer, nullable=False, unique=True)
+    id = Column(Integer, Identity(), nullable=False, unique=True)
     trail_name = Column(String, primary_key=True)
     park_id = Column(Integer, ForeignKey("park.id"), primary_key=True)
     length_miles = Column(Float, nullable=False)
